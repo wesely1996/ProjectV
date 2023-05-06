6f2c360de7f6c39591b2808fa2d6673b6a5105c7
wesely1996 <nikola.wesely.96@gmail.com>
2023-05-06 14:55:52 +0200

    start react app

---
+++ vermunt/src/containers/App/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React{
+
+  render(){
+    return(
+      <div>
+        Hello, World!
+      </div>
+    )
+  }
+}
+
+export default App;
